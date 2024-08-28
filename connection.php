64c98ab599ddbb01795be5ca610f1ce79339e59e
kanishka50 <159050069+kanishka50@users.noreply.github.com>
2024-08-29 04:32:20 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,21 +1,54 @@
-<?php
-
-class Database{
-
+class Database {
     public static $connection;
 
-    public static function setUpConnection(){
-        if(!isset(Database::$connection)){
-            Database::$connection = new mysqli("byteforceinventory.mysql.database.azure.com","teki","Hacker@119","inventorymgt","3306");
+    public static function setUpConnection() {
+        if (!isset(Database::$connection)) {
+            // Define the SSL CA parameter
+            $sslCA = 'ca-cert.pem';
+
+            // Initialize the connection
+            Database::$connection = new mysqli(
+                "byteforceinventory.mysql.database.azure.com", 
+                "teki", 
+                "Hacker@119", 
+                "inventorymgt", 
+                "3306"
+            );
+
+            // Check for connection errors
+            if (Database::$connection->connect_error) {
+                die("Connection failed: " . Database::$connection->connect_error);
+            }
+
+            // Set the SSL CA option
+            Database::$connection->ssl_set(NULL, NULL, $sslCA, NULL, NULL);
+
+            // Enable SSL verification of the server certificate
+            if (!Database::$connection->options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true)) {
+                die("Failed to set SSL verification mode");
+            }
+
+            // Reconnect with SSL enforced
+            if (!Database::$connection->real_connect(
+                "byteforceinventory.mysql.database.azure.com", 
+                "teki", 
+                "Hacker@119", 
+                "inventorymgt", 
+                "3306", 
+                NULL, 
+                MYSQLI_CLIENT_SSL
+            )) {
+                die("SSL connection failed: " . mysqli_connect_error());
+            }
         }
     }
 
-    public static function iud($q){
+    public static function iud($q) {
         Database::setUpConnection();
         Database::$connection->query($q);
     }
 
-    public static function search($q){
+    public static function search($q) {
         Database::setUpConnection();
         $resultset = Database::$connection->query($q);
         return $resultset;
@@ -25,7 +58,5 @@ class Database{
         Database::setUpConnection();
         return Database::$connection;
     }
-
 }
-
 ?>
