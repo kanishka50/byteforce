d8a38e7da8ef81ae81776978549545af74e7c996
kanishka50 <159050069+kanishka50@users.noreply.github.com>
2024-08-29 05:04:33 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -6,17 +6,21 @@ class Database{
 
     public static function setUpConnection(){
         if(!isset(Database::$connection)){
-            Database::$connection = new mysqli("localhost","root","2001kkkK@@","inventorymgt","3306");
+            Database::$connection = new mysqli("byteforceinventory.mysql.database.azure.com","teki","Hacker@119","inventorymgt","3306");
         }
     }
 
     public static function iud($q){
-        Database::setUpConnection();
+        $ssl_ca = '/home/site/wwwroot/ca-cert.pem';
+        Database::$connection->ssl_set(null, null, $ssl_ca, null, null);
+        Database::$connection->real_connect("byteforceinventory.mysql.database.azure.com", "teki", "Hacker@119", "inventorymgt", "3306", null, MYSQLI_CLIENT_SSL);
         Database::$connection->query($q);
     }
 
     public static function search($q){
-        Database::setUpConnection();
+         $ssl_ca = '/home/site/wwwroot/ca-cert.pem';
+        Database::$connection->ssl_set(null, null, $ssl_ca, null, null);
+        Database::$connection->real_connect("byteforceinventory.mysql.database.azure.com", "teki", "Hacker@119", "inventorymgt", "3306", null, MYSQLI_CLIENT_SSL);
         $resultset = Database::$connection->query($q);
         return $resultset;
     }
