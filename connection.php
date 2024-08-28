af68f8478682963f8ba1b5d4c243600baa5969ba
kanishka50 <159050069+kanishka50@users.noreply.github.com>
2024-08-29 05:11:15 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,27 +1,64 @@
 <?php
 
-class Database{
+class Database {
 
     public static $connection;
 
-    public static function setUpConnection(){
-        if(!isset(Database::$connection)){
-            Database::$connection = new mysqli("byteforceinventory.mysql.database.azure.com","teki","Hacker@119","inventorymgt","3306");
+    public static function setUpConnection() {
+        if (!isset(Database::$connection)) {
+            // Define the SSL CA parameter
+            $ssl_ca = '/home/site/wwwroot/ca-cert.pem';
+
+            // Initialize the connection
+            Database::$connection = new mysqli(
+                "byteforceinventory.mysql.database.azure.com", 
+                "teki", 
+                "Hacker@119", 
+                "inventorymgt", 
+                3306 // Port should be an integer
+            );
+
+            // Check for connection errors
+            if (Database::$connection->connect_error) {
+                die("Connection failed: " . Database::$connection->connect_error);
+            }
+
+            // Set the SSL CA option
+            Database::$connection->ssl_set(NULL, NULL, $ssl_ca, NULL, NULL);
+
+            // Enable SSL verification of the server certificate
+            if (!Database::$connection->options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true)) {
+                die("Failed to set SSL verification mode");
+            }
+
+            // Reconnect with SSL enforced
+            if (!Database::$connection->real_connect(
+                "byteforceinventory.mysql.database.azure.com", 
+                "teki", 
+                "Hacker@119", 
+                "inventorymgt", 
+                3306, // Port should be an integer
+                NULL, 
+                MYSQLI_CLIENT_SSL
+            )) {
+                die("SSL connection failed: " . mysqli_connect_error());
+            }
         }
     }
 
-    public static function iud($q){
-        $ssl_ca = '/home/site/wwwroot/ca-cert.pem';
-        Database::$connection->ssl_set(null, null, $ssl_ca, null, null);
-        Database::$connection->real_connect("byteforceinventory.mysql.database.azure.com", "teki", "Hacker@119", "inventorymgt", 3306, null, MYSQLI_CLIENT_SSL);
-        Database::$connection->query($q);
+    public static function iud($q) {
+        Database::setUpConnection();
+        if (!Database::$connection->query($q)) {
+            die("Query failed: " . Database::$connection->error);
+        }
     }
 
-    public static function search($q){
-         $ssl_ca = '/home/site/wwwroot/ca-cert.pem';
-        Database::$connection->ssl_set(null, null, $ssl_ca, null, null);
-        Database::$connection->real_connect("byteforceinventory.mysql.database.azure.com", "teki", "Hacker@119", "inventorymgt", 3306, null, MYSQLI_CLIENT_SSL);
+    public static function search($q) {
+        Database::setUpConnection();
         $resultset = Database::$connection->query($q);
+        if (!$resultset) {
+            die("Query failed: " . Database::$connection->error);
+        }
         return $resultset;
     }
 
